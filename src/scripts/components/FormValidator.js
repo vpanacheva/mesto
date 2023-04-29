f5eefbcfd4403b29c4efc8112ae3eb19bc017a4f
vpanacheva <v_panacheva@mail.ru>
2023-04-29 14:05:21 +0500

    2

--- src/scripts/components/FormValidator.js
+++ src/scripts/components/FormValidator.js
@@ -10,20 +10,24 @@ export class FormValidator {
 
   _showInputError() {
     const errorElement = this._formElement.querySelector(
-      `.${this._inputElement.id}-error`); 
+      `.${this._inputElement.id}-error`
+    ); // находим элемент ошибки внутри самой функции
     this._inputElement.classList.add(this._inputErrorClass);
-    errorElement.textContent = this._inputElement.validationMessage; 
-    errorElement.classList.add(this._errorClass); 
+    errorElement.textContent = this._inputElement.validationMessage; // показываем сообщение об ошибке
+    errorElement.classList.add(this._errorClass); // замена содержимого span с ошибкой на переданный параметр
   }
 
+  /** функция, которая удаляет класс с ошибкой */
   _hideInputError() {
     const errorElement = this._formElement.querySelector(
-      `.${this._inputElement.id}-error`); 
+      `.${this._inputElement.id}-error`
+    ); // находим элемент ошибки
     this._inputElement.classList.remove(this._inputErrorClass);
     errorElement.classList.add(this._errorClass); //
-    errorElement.textContent = ''; 
+    errorElement.textContent = ''; // cкрываем сообщение об ошибке
   }
 
+  /** функция, которая проверяет валидность поля. Принимает inputElement */
   _isValid(inputElement) {
     if (!this._inputElement.validity.valid) {
       this._showInputError(inputElement);
@@ -32,17 +36,16 @@ export class FormValidator {
     }
   }
 
+  /** функция, которая проверяет валидность полей и отключает или включает кнопку отправки */
   _toggleButtonState() {
     const isFormValid = this._formElement.checkValidity();
     this._buttonElement.disabled = !isFormValid;
     this._buttonElement.classList.toggle(
-      this._inactiveButtonClass, 
-      !isFormValid 
+      this._inactiveButtonClass, // добавляем класс неактивной кнопки
+      !isFormValid // если валидация не пройдена
     );
   }
-  _stopSubmit = (e) => {
-    e.preventDefault();
-}
+
   _setEventListeners() {
     this._inputList = this._formElement.querySelectorAll(this._inputSelector);
     this._inputList.forEach((inputElement) => {
@@ -53,9 +56,12 @@ export class FormValidator {
       });
     });
 
-    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
+    this._buttonElement = this._formElement.querySelector(
+      this._submitButtonSelector
+    );
   }
 
+  /** функция проверки формы -> деактивация кнопки и удаление текста ошибки */
   disablesSubmitForm() {
     this._inputList.forEach((inputElement) => {
       this._inputElement = inputElement;
